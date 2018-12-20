e3ce677f4936823026de2feb8eec2cb5ff082609
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-20 16:20:01 +0800

    susun semula koding

--- register.php
+++ register.php
@@ -1,38 +1,36 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] =='POST')
+{
+	if (isset($_POST['name']))
+	{
+		$name = $_POST['name'];
+	}
+	if (isset($_POST['email']))
+	{
+		$email = $_POST['email'];
+	}
+	if (isset($_POST['password']))
+	{
+		$password = $_POST['password'];
+	}
 
-if ($_SERVER['REQUEST_METHOD'] =='POST'){
+	require_once 'connect.php';
+	$sql = "INSERT INTO users_table (name, email, password) VALUES ('$name', '$email', '$password')";
+	//echo '<pre>$sql='; print_r($sql); echo '</pre>';
 
-    if (isset($_POST['name'])) {
-    $name = $_POST['name'];
+	if ( mysqli_query($conn, $sql) )
+	{
+		$result["success"] = "1";
+		$result["message"] = "success";
+		echo json_encode($result);
+		mysqli_close($conn);
+	}
+	else
+	{
+		$result["success"] = "0";
+		$result["message"] = "error";
+		echo json_encode($result);
+		mysqli_close($conn);
+	}//*/
 }
- if (isset($_POST['email'])) {
-    $email = $_POST['email'];
-}
- if (isset($_POST['password'])) {
-    $password = $_POST['password'];
-}
-
-
-
-    require_once 'connect.php';
-
-    $sql = "INSERT INTO users_table (name, email, password) VALUES ('$name', '$email', '$password')";
-
-    if ( mysqli_query($conn, $sql) ) {
-        $result["success"] = "1";
-        $result["message"] = "success";
-
-        echo json_encode($result);
-        mysqli_close($conn);
-
-    } else {
-
-        $result["success"] = "0";
-        $result["message"] = "error";
-
-        echo json_encode($result);
-        mysqli_close($conn);
-    }
-}
-
 ?>
