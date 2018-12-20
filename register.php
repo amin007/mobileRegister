f073d2185df17ee54a383fbf43b5ff215ee93823
shazlinaziz <izz.shazlin@gmail.com>
2018-12-20 13:45:10 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] =='POST'){
+
+    if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+}
+ if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+}
+ if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+}
+
+
+
+    require_once 'connect.php';
+
+    $sql = "INSERT INTO users_table (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ( mysqli_query($conn, $sql) ) {
+        $result["success"] = "1";
+        $result["message"] = "success";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+
+    } else {
+
+        $result["success"] = "0";
+        $result["message"] = "error";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+}
+
+?>
